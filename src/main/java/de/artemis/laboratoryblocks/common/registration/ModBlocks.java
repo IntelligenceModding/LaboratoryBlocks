97977c97481b013c47751ce322284e4ec63f93e9
Artemis <mattigrego@gmx.de>
2022-12-19 21:20:31 +0100

    - Reworked every texture - Enlighted Laboratory Glass no longer connects to Laboratory Glass

--- src/main/java/de/artemis/laboratoryblocks/common/registration/ModBlocks.java
+++ src/main/java/de/artemis/laboratoryblocks/common/registration/ModBlocks.java
@@ -9,8 +9,6 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
