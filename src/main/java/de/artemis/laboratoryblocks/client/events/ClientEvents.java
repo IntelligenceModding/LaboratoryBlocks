7ed154bafb30d38d5d6c5af224a1bdbb54309906
Artemis <mattigrego@gmx.de>
2023-01-15 21:25:28 +0100

    - Added details to the mods.toml

--- src/main/java/de/artemis/laboratoryblocks/client/events/ClientEvents.java
+++ src/main/java/de/artemis/laboratoryblocks/client/events/ClientEvents.java
@@ -5,12 +5,14 @@ import de.artemis.laboratoryblocks.common.registration.ModBlocks;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = LaboratoryBlocks.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientEvents {
 
+    @SubscribeEvent
     public static void onStartUpEvent (FMLClientSetupEvent event) {
         ItemBlockRenderTypes.setRenderLayer(ModBlocks.LABORATORY_GLASS.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(ModBlocks.ENLIGHTED_LABORATORY_GLASS.get(), RenderType.cutout());
