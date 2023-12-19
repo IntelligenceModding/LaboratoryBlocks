feadacae2f15580bfe129715694617a8b4caf7f4
Srendi <info@denizdemiroglu.de>
2023-12-19 12:38:46 +0100

    Use the modrinth maven instead of cursemaven and update fusion to fix transparency

--- src/main/java/de/artemis/laboratoryblocks/LaboratoryBlocks.java
+++ src/main/java/de/artemis/laboratoryblocks/LaboratoryBlocks.java
@@ -1,35 +1,18 @@
 package de.artemis.laboratoryblocks;
 
 import com.mojang.logging.LogUtils;
-import de.artemis.laboratoryblocks.common.registration.ModBlocks;
-import de.artemis.laboratoryblocks.common.registration.ModItems;
 import de.artemis.laboratoryblocks.common.registration.Registration;
-import net.minecraft.core.NonNullList;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.ItemStackedOnOtherEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 @Mod(LaboratoryBlocks.MOD_ID)
 public class LaboratoryBlocks {
 
     public static final String MOD_ID = "laboratoryblocks";
     private static final Logger LOGGER = LogUtils.getLogger();
-    
+
     public LaboratoryBlocks() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
         Registration.register();
     }
